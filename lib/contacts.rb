13b9cc79b924231962da441afbbde8a10acedc94
Tim Knoll <timothy.knoll@gmail.com>
2019-12-30 21:34:10 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,18 +14,15 @@
   #   }
   # }
 
-# def remove_strawberry(contacts)
-#   contacts.each do |person, contact_details_hash|
-#     contact_details_hash.each do |attribute, data|
-#       if attribute == :favorite_ice_cream_flavors
-#         data.delete_if {|ice_cream| ice_cream == "strawberry"}
-#       end
-#     end
-#   end
-# end
-
 def remove_strawberry(contacts)
-  contacts.each do |p, d|
-    binding.pry 
-  end   
-end   
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
+end
+
+
+   
